bf0affe6b8d9adde1fe493d9011625158c7f6d93
woldemortq <valeriy_valeriy322@mail.ru>
2025-07-17 17:21:22 +0500

    Add fetch and colors with dates

--- main.js
+++ main.js
@@ -1,31 +1,50 @@
 const container = document.querySelector('.container')
 
-for (let column = 0; column < 51; column++) {
-    const col = document.createElement('div')
-    col.classList.add('week')
+async function init() {
+    const data = await fetchData();
+
+    const dates = Object.keys(data).sort();
+
+    for (let column = 0; column < 51; column++) {
+        const col = document.createElement('div');
+        col.classList.add('week');
+
+        for (let row = 0; row < 7; row++) {
+            const dayIndex = column * 7 + row;
+            const day = document.createElement('div');
+            day.classList.add('days');
+
+            const dateStr = dates[dayIndex];
+            if (dateStr) {
+                const value = data[dateStr];
 
 
-    for (let row = 0; row < 7; row++){
-        const day = document.createElement('div')
-        day.classList.add('days')
-        col.appendChild(day)
+                day.setAttribute('data-date', dateStr);
+                day.setAttribute('title', `Дата: ${dateStr}\nЗначение: ${value}`);
+
+                if (value === 0) {
+                    day.style.backgroundColor = '#EDEDED';
+                } else if (value >= 1 && value <= 9) {
+                    day.style.backgroundColor = '#ACD5F2';
+                } else if (value >= 10 && value <= 19) {
+                    day.style.backgroundColor = '#7FA8C9';
+                } else if (value >= 20 && value <= 29) {
+                    day.style.backgroundColor = '#527BA0';
+                } else if (value >= 30) {
+                    day.style.backgroundColor = '#254E77';
+                }
+
+
+            }
+            col.appendChild(day);
+        }
+        container.appendChild(col);
     }
-    container.appendChild(col)
 }
 
-const dates = []
+init();
 
-const today = new Date()
-
-const WeekDay = today.getDay()
-const Sunday = new Date(today)
-Sunday.setDate(today.getDate() - WeekDay)
-
-const startDate = new Date(Sunday)
-startDate.setDate(Sunday.getDate() - (51 * 7 -1))
-
-for (let i = 0; i < 357; i++) {
-    const date = new Date(startDate)
-    date.setDate(startDate.getDate() + i)
-    dates.push(date)
+async function fetchData() {
+    const res = await fetch('https://dpg.gg/test/calendar.json');
+    return await res.json();
 }

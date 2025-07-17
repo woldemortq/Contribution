837d312f9a2abea4ecaf7df2fc2a780195ef38f1
woldemortq <valeriy_valeriy322@mail.ru>
2025-07-17 16:27:11 +0500

    Add main.js & css

--- main.js
+++ main.js
@@ -12,3 +12,20 @@ for (let column = 0; column < 51; column++) {
     }
     container.appendChild(col)
 }
+
+const dates = []
+
+const today = new Date()
+
+const WeekDay = today.getDay()
+const Sunday = new Date(today)
+Sunday.setDate(today.getDate() - WeekDay)
+
+const startDate = new Date(Sunday)
+startDate.setDate(Sunday.getDate() - (51 * 7 -1))
+
+for (let i = 0; i < 357; i++) {
+    const date = new Date(startDate)
+    date.setDate(startDate.getDate() + i)
+    dates.push(date)
+}
